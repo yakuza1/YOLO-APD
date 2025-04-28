961816807bd83fd8b477ce10196f8cb4da34a281
Aquino Nyapara <32511975+yakuza1@users.noreply.github.com>
2025-04-28 14:57:13 +0300

    Update conv.py

--- ultralytics/nn/modules/conv.py
+++ ultralytics/nn/modules/conv.py
@@ -241,118 +241,347 @@ class ConvTranspose(nn.Module):
         return self.act(self.conv_transpose(x))
 
 
-class DSConv(_ConvNd):
+class DSConv(nn.Module):
     def __init__(
-        self,
-        in_channels,
-        out_channels,
-        kernel_size,
-        stride=1,
-        padding=None,
-        dilation=1,
-        groups=1,
-        padding_mode="zeros",
-        bias=False,
-        block_size=32,
-        KDSBias=False,
-        CDS=False,
+        self, in_ch, out_ch, morph, kernel_size=3, if_offset=True, extend_scope=1
     ):
-        padding = _pair(autopad(kernel_size, padding, dilation))
-        kernel_size = _pair(kernel_size)
-        stride = _pair(stride)
-        dilation = _pair(dilation)
+        """
+        The Dynamic Snake Convolution
+        :param in_ch: input channel
+        :param out_ch: output channel
+        :param kernel_size: the size of kernel
+        :param extend_scope: the range to expand (default 1 for this method)
+        :param morph: the morphology of the convolution kernel is mainly divided into two types
+                        along the x-axis (0) and the y-axis (1) (see the paper for details)
+        :param if_offset: whether deformation is required, if it is False, it is the standard convolution kernel
+        """
+        super(DSConv, self).__init__()
+        # use the <offset_conv> to learn the deformable offset
+        self.offset_conv = nn.Conv2d(in_ch, 2 * kernel_size, 3, padding=1)
+        self.bn = nn.BatchNorm2d(2 * kernel_size)
+        self.kernel_size = kernel_size
+        # two types of the DSConv (along x-axis and y-axis)
+        self.dsc_conv_x = nn.Conv2d(
+            in_ch,
+            out_ch,
+            kernel_size=(kernel_size, 1),
+            stride=(kernel_size, 1),
+            padding=0,
+        )
+        self.dsc_conv_y = nn.Conv2d(
+            in_ch,
+            out_ch,
+            kernel_size=(1, kernel_size),
+            stride=(1, kernel_size),
+            padding=0,
+        )
+        self.gn = nn.GroupNorm(out_ch // 4, out_ch)
+        self.act = Conv.default_act
+        self.extend_scope = extend_scope
+        self.morph = morph
+        self.if_offset = if_offset
 
-        blck_numb = math.ceil(((in_channels) / (block_size * groups)))
-        super(DSConv, self).__init__(
-            in_channels,
-            out_channels,
-            kernel_size,
-            stride,
-            padding,
-            dilation,
-            False,
-            _pair(0),
-            groups,
-            bias,
-            padding_mode,
+    def forward(self, f):
+        offset = self.offset_conv(f)
+        offset = self.bn(offset)
+        # We need a range of deformation between -1 and 1 to mimic the snake's swing
+        offset = torch.tanh(offset)
+        input_shape = f.shape
+        dsc = DSC(input_shape, self.kernel_size, self.extend_scope, self.morph)
+        deformed_feature = dsc.deform_conv(f, offset, self.if_offset)
+        if self.morph == 0:
+            x = self.dsc_conv_x(deformed_feature.type(f.dtype))
+            x = self.gn(x)
+            x = self.act(x)
+            return x
+        else:
+            x = self.dsc_conv_y(deformed_feature.type(f.dtype))
+            x = self.gn(x)
+            x = self.act(x)
+            return x
+
+
+# Core code, for ease of understanding, we mark the dimensions of input and output next to the code
+class DSC(object):
+    def __init__(self, input_shape, kernel_size, extend_scope, morph):
+        self.num_points = kernel_size
+        self.width = input_shape[2]
+        self.height = input_shape[3]
+        self.morph = morph
+        self.extend_scope = extend_scope  # offset (-1 ~ 1) * extend_scope
+        # define feature map shape
+        """
+        B: Batch size  C: Channel  W: Width  H: Height
+        """
+        self.num_batch = input_shape[0]
+        self.num_channels = input_shape[1]
+
+    """
+    input: offset [B,2*K,W,H]  K: Kernel size (2*K: 2D image, deformation contains <x_offset> and <y_offset>)
+    output_x: [B,1,W,K*H]   coordinate map
+    output_y: [B,1,K*W,H]   coordinate map
+    """
+
+    def _coordinate_map_3D(self, offset, if_offset):
+        device = offset.device
+        # offset
+        y_offset, x_offset = torch.split(offset, self.num_points, dim=1)
+        y_center = torch.arange(0, self.width).repeat([self.height])
+        y_center = y_center.reshape(self.height, self.width)
+        y_center = y_center.permute(1, 0)
+        y_center = y_center.reshape([-1, self.width, self.height])
+        y_center = y_center.repeat([self.num_points, 1, 1]).float()
+        y_center = y_center.unsqueeze(0)
+        x_center = torch.arange(0, self.height).repeat([self.width])
+        x_center = x_center.reshape(self.width, self.height)
+        x_center = x_center.permute(0, 1)
+        x_center = x_center.reshape([-1, self.width, self.height])
+        x_center = x_center.repeat([self.num_points, 1, 1]).float()
+        x_center = x_center.unsqueeze(0)
+        if self.morph == 0:
+            """
+            Initialize the kernel and flatten the kernel
+                y: only need 0
+                x: -num_points//2 ~ num_points//2 (Determined by the kernel size)
+                !!! The related PPT will be submitted later, and the PPT will contain the whole changes of each step
+            """
+            y = torch.linspace(0, 0, 1)
+            x = torch.linspace(
+                -int(self.num_points // 2),
+                int(self.num_points // 2),
+                int(self.num_points),
+            )
+            y, x = torch.meshgrid(y, x)
+            y_spread = y.reshape(-1, 1)
+            x_spread = x.reshape(-1, 1)
+            y_grid = y_spread.repeat([1, self.width * self.height])
+            y_grid = y_grid.reshape([self.num_points, self.width, self.height])
+            y_grid = y_grid.unsqueeze(0)  # [B*K*K, W,H]
+            x_grid = x_spread.repeat([1, self.width * self.height])
+            x_grid = x_grid.reshape([self.num_points, self.width, self.height])
+            x_grid = x_grid.unsqueeze(0)  # [B*K*K, W,H]
+            y_new = y_center + y_grid
+            x_new = x_center + x_grid
+            y_new = y_new.repeat(self.num_batch, 1, 1, 1).to(device)
+            x_new = x_new.repeat(self.num_batch, 1, 1, 1).to(device)
+            y_offset_new = y_offset.detach().clone()
+            if if_offset:
+                y_offset = y_offset.permute(1, 0, 2, 3)
+                y_offset_new = y_offset_new.permute(1, 0, 2, 3)
+                center = int(self.num_points // 2)
+                # The center position remains unchanged and the rest of the positions begin to swing
+                # This part is quite simple. The main idea is that "offset is an iterative process"
+                y_offset_new[center] = 0
+                for index in range(1, center):
+                    y_offset_new[center + index] = (
+                        y_offset_new[center + index - 1] + y_offset[center + index]
+                    )
+                    y_offset_new[center - index] = (
+                        y_offset_new[center - index + 1] + y_offset[center - index]
+                    )
+                y_offset_new = y_offset_new.permute(1, 0, 2, 3).to(device)
+                y_new = y_new.add(y_offset_new.mul(self.extend_scope))
+            y_new = y_new.reshape(
+                [self.num_batch, self.num_points, 1, self.width, self.height]
+            )
+            y_new = y_new.permute(0, 3, 1, 4, 2)
+            y_new = y_new.reshape(
+                [self.num_batch, self.num_points * self.width, 1 * self.height]
+            )
+            x_new = x_new.reshape(
+                [self.num_batch, self.num_points, 1, self.width, self.height]
+            )
+            x_new = x_new.permute(0, 3, 1, 4, 2)
+            x_new = x_new.reshape(
+                [self.num_batch, self.num_points * self.width, 1 * self.height]
+            )
+            return y_new, x_new
+        else:
+            """
+            Initialize the kernel and flatten the kernel
+                y: -num_points//2 ~ num_points//2 (Determined by the kernel size)
+                x: only need 0
+            """
+            y = torch.linspace(
+                -int(self.num_points // 2),
+                int(self.num_points // 2),
+                int(self.num_points),
+            )
+            x = torch.linspace(0, 0, 1)
+            y, x = torch.meshgrid(y, x)
+            y_spread = y.reshape(-1, 1)
+            x_spread = x.reshape(-1, 1)
+            y_grid = y_spread.repeat([1, self.width * self.height])
+            y_grid = y_grid.reshape([self.num_points, self.width, self.height])
+            y_grid = y_grid.unsqueeze(0)
+            x_grid = x_spread.repeat([1, self.width * self.height])
+            x_grid = x_grid.reshape([self.num_points, self.width, self.height])
+            x_grid = x_grid.unsqueeze(0)
+            y_new = y_center + y_grid
+            x_new = x_center + x_grid
+            y_new = y_new.repeat(self.num_batch, 1, 1, 1)
+            x_new = x_new.repeat(self.num_batch, 1, 1, 1)
+            y_new = y_new.to(device)
+            x_new = x_new.to(device)
+            x_offset_new = x_offset.detach().clone()
+            if if_offset:
+                x_offset = x_offset.permute(1, 0, 2, 3)
+                x_offset_new = x_offset_new.permute(1, 0, 2, 3)
+                center = int(self.num_points // 2)
+                x_offset_new[center] = 0
+                for index in range(1, center):
+                    x_offset_new[center + index] = (
+                        x_offset_new[center + index - 1] + x_offset[center + index]
+                    )
+                    x_offset_new[center - index] = (
+                        x_offset_new[center - index + 1] + x_offset[center - index]
+                    )
+                x_offset_new = x_offset_new.permute(1, 0, 2, 3).to(device)
+                x_new = x_new.add(x_offset_new.mul(self.extend_scope))
+            y_new = y_new.reshape(
+                [self.num_batch, 1, self.num_points, self.width, self.height]
+            )
+            y_new = y_new.permute(0, 3, 1, 4, 2)
+            y_new = y_new.reshape(
+                [self.num_batch, 1 * self.width, self.num_points * self.height]
+            )
+            x_new = x_new.reshape(
+                [self.num_batch, 1, self.num_points, self.width, self.height]
+            )
+            x_new = x_new.permute(0, 3, 1, 4, 2)
+            x_new = x_new.reshape(
+                [self.num_batch, 1 * self.width, self.num_points * self.height]
+            )
+            return y_new, x_new
+
+    """
+    input: input feature map [N,C,D,W,H]；coordinate map [N,K*D,K*W,K*H] 
+    output: [N,1,K*D,K*W,K*H]  deformed feature map
+    """
+
+    def _bilinear_interpolate_3D(self, input_feature, y, x):
+        device = input_feature.device
+        y = y.reshape([-1]).float()
+        x = x.reshape([-1]).float()
+        zero = torch.zeros([]).int()
+        max_y = self.width - 1
+        max_x = self.height - 1
+        # find 8 grid locations
+        y0 = torch.floor(y).int()
+        y1 = y0 + 1
+        x0 = torch.floor(x).int()
+        x1 = x0 + 1
+        # clip out coordinates exceeding feature map volume
+        y0 = torch.clamp(y0, zero, max_y)
+        y1 = torch.clamp(y1, zero, max_y)
+        x0 = torch.clamp(x0, zero, max_x)
+        x1 = torch.clamp(x1, zero, max_x)
+        input_feature_flat = input_feature.flatten()
+        input_feature_flat = input_feature_flat.reshape(
+            self.num_batch, self.num_channels, self.width, self.height
+        )
+        input_feature_flat = input_feature_flat.permute(0, 2, 3, 1)
+        input_feature_flat = input_feature_flat.reshape(-1, self.num_channels)
+        dimension = self.height * self.width
+        base = torch.arange(self.num_batch) * dimension
+        base = base.reshape([-1, 1]).float()
+        repeat = torch.ones([self.num_points * self.width * self.height]).unsqueeze(0)
+        repeat = repeat.float()
+        base = torch.matmul(base, repeat)
+        base = base.reshape([-1])
+        base = base.to(device)
+        base_y0 = base + y0 * self.height
+        base_y1 = base + y1 * self.height
+        # top rectangle of the neighbourhood volume
+        index_a0 = base_y0 - base + x0
+        index_c0 = base_y0 - base + x1
+        # bottom rectangle of the neighbourhood volume
+        index_a1 = base_y1 - base + x0
+        index_c1 = base_y1 - base + x1
+        # get 8 grid values
+        value_a0 = input_feature_flat[index_a0.type(torch.int64)].to(device)
+        value_c0 = input_feature_flat[index_c0.type(torch.int64)].to(device)
+        value_a1 = input_feature_flat[index_a1.type(torch.int64)].to(device)
+        value_c1 = input_feature_flat[index_c1.type(torch.int64)].to(device)
+        # find 8 grid locations
+        y0 = torch.floor(y).int()
+        y1 = y0 + 1
+        x0 = torch.floor(x).int()
+        x1 = x0 + 1
+        # clip out coordinates exceeding feature map volume
+        y0 = torch.clamp(y0, zero, max_y + 1)
+        y1 = torch.clamp(y1, zero, max_y + 1)
+        x0 = torch.clamp(x0, zero, max_x + 1)
+        x1 = torch.clamp(x1, zero, max_x + 1)
+        x0_float = x0.float()
+        x1_float = x1.float()
+        y0_float = y0.float()
+        y1_float = y1.float()
+        vol_a0 = ((y1_float - y) * (x1_float - x)).unsqueeze(-1).to(device)
+        vol_c0 = ((y1_float - y) * (x - x0_float)).unsqueeze(-1).to(device)
+        vol_a1 = ((y - y0_float) * (x1_float - x)).unsqueeze(-1).to(device)
+        vol_c1 = ((y - y0_float) * (x - x0_float)).unsqueeze(-1).to(device)
+        outputs = (
+            value_a0 * vol_a0
+            + value_c0 * vol_c0
+            + value_a1 * vol_a1
+            + value_c1 * vol_c1
+        )
+        if self.morph == 0:
+            outputs = outputs.reshape(
+                [
+                    self.num_batch,
+                    self.num_points * self.width,
+                    1 * self.height,
+                    self.num_channels,
+                ]
+            )
+            outputs = outputs.permute(0, 3, 1, 2)
+        else:
+            outputs = outputs.reshape(
+                [
+                    self.num_batch,
+                    1 * self.width,
+                    self.num_points * self.height,
+                    self.num_channels,
+                ]
+            )
+            outputs = outputs.permute(0, 3, 1, 2)
+        return outputs
+
+    def deform_conv(self, input, offset, if_offset):
+        y, x = self._coordinate_map_3D(offset, if_offset)
+        deformed_feature = self._bilinear_interpolate_3D(input, y, x)
+        return deformed_feature
+
+
+class Bottleneck_DySnakeConv(Bottleneck):
+    """Standard bottleneck with DySnakeConv."""
+
+    def __init__(
+        self, c1, c2, shortcut=True, g=1, k=(3, 3), e=0.5
+    ):  # ch_in, ch_out, shortcut, groups, kernels, expand
+        super().__init__(c1, c2, shortcut, g, k, e)
+        c_ = int(c2 * e)  # hidden channels
+        self.cv2 = DySnakeConv(c_, c2, k[1])
+        self.cv3 = Conv(c2 * 3, c2, k=1)
+
+    def forward(self, x):
+        """'forward()' applies the YOLOv5 FPN to input data."""
+        return (
+            x + self.cv3(self.cv2(self.cv1(x)))
+            if self.add
+            else self.cv3(self.cv2(self.cv1(x)))
         )
 
-        # KDS weight From Paper
-        self.intweight = torch.Tensor(out_channels, in_channels, *kernel_size)
-        self.alpha = torch.Tensor(out_channels, blck_numb, *kernel_size)
 
-        # KDS bias From Paper
-        self.KDSBias = KDSBias
-        self.CDS = CDS
-
-        if KDSBias:
-            self.KDSb = torch.Tensor(out_channels, blck_numb, *kernel_size)
-        if CDS:
-            self.CDSw = torch.Tensor(out_channels)
-            self.CDSb = torch.Tensor(out_channels)
-
-        self.reset_parameters()
-
-    def get_weight_res(self):
-        # Include expansion of alpha and multiplication with weights to include in the convolution layer here
-        alpha_res = torch.zeros(self.weight.shape).to(self.alpha.device)
-
-        # Include KDSBias
-        if self.KDSBias:
-            KDSBias_res = torch.zeros(self.weight.shape).to(self.alpha.device)
-
-        # Handy definitions:
-        nmb_blocks = self.alpha.shape[1]
-        total_depth = self.weight.shape[1]
-        bs = total_depth // nmb_blocks
-
-        llb = total_depth - (nmb_blocks - 1) * bs
-
-        # Casting the Alpha values as same tensor shape as weight
-        for i in range(nmb_blocks):
-            length_blk = llb if i == nmb_blocks - 1 else bs
-
-            shp = self.alpha.shape  # Notice this is the same shape for the bias as well
-            to_repeat = self.alpha[:, i, ...].view(shp[0], 1, shp[2], shp[3]).clone()
-            repeated = to_repeat.expand(shp[0], length_blk, shp[2], shp[3]).clone()
-            alpha_res[:, i * bs : (i * bs + length_blk), ...] = repeated.clone()
-
-            if self.KDSBias:
-                to_repeat = self.KDSb[:, i, ...].view(shp[0], 1, shp[2], shp[3]).clone()
-                repeated = to_repeat.expand(shp[0], length_blk, shp[2], shp[3]).clone()
-                KDSBias_res[:, i * bs : (i * bs + length_blk), ...] = repeated.clone()
-
-        if self.CDS:
-            to_repeat = self.CDSw.view(-1, 1, 1, 1)
-            repeated = to_repeat.expand_as(self.weight)
-            print(repeated.shape)
-
-        # Element-wise multiplication of alpha and weight
-        weight_res = torch.mul(alpha_res, self.weight)
-        if self.KDSBias:
-            weight_res = torch.add(weight_res, KDSBias_res)
-        return weight_res
-
-    def forward(self, input):
-        # Get resulting weight
-        # weight_res = self.get_weight_res()
-
-        # Returning convolution
-        return F.conv2d(
-            input,
-            self.weight,
-            self.bias,
-            self.stride,
-            self.padding,
-            self.dilation,
-            self.groups,
+class C2f_DySnakeConv(C2f):
+    def __init__(self, c1, c2, n=1, shortcut=False, g=1, e=0.5):
+        super().__init__(c1, c2, n, shortcut, g, e)
+        self.m = nn.ModuleList(
+            Bottleneck_DySnakeConv(self.c, self.c, shortcut, g, k=(3, 3), e=1.0)
+            for _ in range(n)
         )
-
-
-class DSConv2D(Conv):
-    def __init__(self, inc, ouc, k=1, s=1, p=None, g=1, d=1, act=True):
-        super().__init__(inc, ouc, k, s, p, g, d, act)
-        self.conv = DSConv(inc, ouc, k, s, p, g, d)
 
 
 class Bottleneck_DSConv(nn.Module):
